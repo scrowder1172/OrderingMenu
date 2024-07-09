42c68158e34c2dbdf14728f457464eefe3dd0f0b
Scott Crowder <scrowder@gmail.com>
2024-07-09 11:42:54 -0500

    Initial Commit

---
+++ OrderingMenu/OrderingMenuApp.swift
@@ -0,0 +1,20 @@
+//
+// File: OrderingMenuApp.swift
+// Project: OrderingMenu
+// 
+// Created by SCOTT CROWDER on 7/9/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct OrderingMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
